8527008ae1beddabf6debf638eb2e1cd3c596ffe
jo2907 <qh20364@bristol.ac.uk>
2023-01-27 13:27:23 +0000

    exercise 2a

--- Exercise2.py
+++ Exercise2.py
@@ -5,4 +5,13 @@ Created on Fri Jan 27 11:58:42 2023
 
 @author: joparker
 """
+import numpy as np 
+a = [1,2,3]
+b = [6,5,4]
+avec = np.array(a)
+bvec = np.array(b)
+def dotvec(a,b):
+    dp = np.dot(a,b)
+    return dp
 
+print(dotvec(avec,bvec))

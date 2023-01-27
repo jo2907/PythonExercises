2b90ecd91fb7ab744b4684ca46297c6ebb2f37de
jo2907 <qh20364@bristol.ac.uk>
2023-01-27 13:50:55 +0000

    q2b

--- Exercise2.py
+++ Exercise2.py
@@ -6,12 +6,28 @@ Created on Fri Jan 27 11:58:42 2023
 @author: joparker
 """
 import numpy as np 
-a = [1,2,3]
+# Q2a - dot product of two vectors
+a = [1,2]
 b = [6,5,4]
 avec = np.array(a)
 bvec = np.array(b)
 def dotvec(a,b):
-    dp = np.dot(a,b)
-    return dp
+    if len(a) == len(b):
+        dp = np.dot(a,b)
+        return dp
+    else:
+        print("Error: vector lengths must be the same")
+        
+       
 
 print(dotvec(avec,bvec))
+
+# Q2b - product of matrices
+A = np.array([ [1, 2],[3,4]]) 
+B = np.array([ [5, 6],[7,8]]) 
+def matrixmultip(a,b):
+    # if no. of rows of a == no. of cols of b
+    product = np.matmul(a,b)
+    return product
+
+print(matrixmultip(A,B))

59b9d814290579cf49f90a6e832f4ecdf43b7d8f
jo2907 <qh20364@bristol.ac.uk>
2023-01-29 14:45:08 +0000

    changes to plot

--- Exercise6.py
+++ Exercise6.py
@@ -3,5 +3,10 @@ import matplotlib.pyplot as plt
 
 t = np.array(np.linspace(0,5,num=500))
 y = (t**2)*np.exp(-2*t)
-plt.plot(t,y)
+plt.plot(t,y,'r-', linewidth=4)
+
+# add labels
+plt.xlabel("x values")
+plt.ylabel("y")
+
 plt.show()
